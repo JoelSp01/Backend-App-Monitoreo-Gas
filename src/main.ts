009539b73dcd8a80d83a50e904607769cba19c95
Joelsp15 <joeldesarrollador@gmail.com>
2024-04-08 00:52:40 -0500

    feat: insert data in Bdd

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(MqttModule, {
     transport: Transport.MQTT,
     options: {
-      url: 'mqtt://172.16.131.165:1883',
+      url: 'mqtt://192.168.1.11:1883',
     },
   });
 }
--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { MqttClient, connect } from 'mqtt';
+import * as mysql from 'mysql';
 
 @Injectable()
 export class MqttService {
     private readonly client: MqttClient;
+    private readonly dbConnection: mysql.Connection;
 
     constructor() {
         // Inicializa el cliente MQTT con la URL del broker
         this.client = connect('mqtt://192.168.1.11:1883', {
             clientId: 'nestjs-mqtt-client'
+        });
+
+        // Inicializa la conexión a la base de datos MySQL
+        this.dbConnection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'bdd_titulacion'
         });
 
         // Maneja los eventos de conexión y mensaje recibido
@@ -34,5 +44,24 @@ export class MqttService {
     handleMessage(topic: string, message: string) {
         // Implementa el manejo de mensajes si es necesario
         console.log(`Mensaje recibido en el topic ${topic}: ${message}`);
+
+        try {
+            // Parsea el mensaje JSON recibido
+            const data = JSON.parse(message);
+
+            // Inserta los datos en la base de datos MySQL
+            const query = `INSERT INTO tbl_lecturas (lec_peso, lec_temperatura) VALUES (?, ?)`;
+            this.dbConnection.query(query, [data.peso, data.temperatura], (error, results, fields) => {
+                if (error) {
+                    console.error('Error al insertar datos en la base de datos:', error);
+                } else {
+                    console.log('Datos insertados correctamente en la base de datos.');
+                }
+            });
+        } catch (error) {
+            console.error('Error al parsear el mensaje JSON:', error);
+        }
     }
+
+
 }
